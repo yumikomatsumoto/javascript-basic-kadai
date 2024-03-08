f540c062c1af23648f4b1441f35cf548487837fc
Mattsun <39019388+yumikomatsumoto@users.noreply.github.com>
2024-03-08 12:00:36 +0900

    Add files via upload

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -4,5 +4,8 @@ let weight　= 68;;
 // 変数の宣言・値の代入(身長)
 let height = 1.7;
 
-// 計算式
-console.log(weight / (height * height));
+//BMI値算出
+let bmi = weight / (height * height)
+
+// 結果表示
+console.log(bmi);
