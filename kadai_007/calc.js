09c0826fdf8b0db16375e43b92568beccbc276cc
Mattsun <39019388+yumikomatsumoto@users.noreply.github.com>
2024-03-08 18:23:00 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,9 +1,6 @@
 // 変数に0~30までのランダムな整数を代入する
 let num = Math.floor(Math.random() * 31);
 
-// 変数numの値を出力する
-console.log(num);
-
 // 条件分岐
 if(num % 15 === 0 ) {
   console.log('3と5の倍数です');
@@ -16,6 +13,6 @@ else if(num % 5 === 0) {
   console.log('5の倍数です');
 }
 
-else{
-  console.log('num');
+else {
+  console.log(num);
 }
