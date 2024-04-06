5926869df0f19c54e22938530852295ef7350d7f
Mattsun <39019388+yumikomatsumoto@users.noreply.github.com>
2024-04-06 11:30:14 +0900

    Add files via upload

--- kadai_020/event.js
+++ kadai_020/event.js
@@ -1,5 +1,8 @@
-// btnというidを持つHTML要素を取得し、定数に代入する
+// btnのidを持つHTML要素を取得し、定数に代入する
 const btn = document.getElementById('btn');
+
+// textのidを持つHTML要素を取得し、定数に代入する
+const text = document.getElementById('text');
 
 // HTML要素がクリックされたときにイベント処理を実行する
 btn.addEventListener('click', () => {
